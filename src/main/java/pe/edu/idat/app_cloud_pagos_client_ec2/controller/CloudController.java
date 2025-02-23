f18ae324386f06d7e523179ede9c4968c40c3257
JoseRaul14 <norona.gomez.jos@gmail.com>
2025-02-23 15:59:07 -0500

    este commit pertenece a jose noroña

--- src/main/java/pe/edu/idat/app_cloud_pagos_client_ec2/controller/CloudController.java
+++ src/main/java/pe/edu/idat/app_cloud_pagos_client_ec2/controller/CloudController.java
@@ -1,13 +1,9 @@
 package pe.edu.idat.app_cloud_pagos_client_ec2.controller;
-
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
 
 public class CloudController {
     @Value("${mensaje:Default mensaje}")
