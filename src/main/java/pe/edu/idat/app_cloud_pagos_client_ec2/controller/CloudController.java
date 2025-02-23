f9d3534e5853129b4958dfa94f09244f185a735c
JoseRaul14 <norona.gomez.jos@gmail.com>
2025-02-23 16:12:56 -0500

    este commit pertenece a jose noroña

--- src/main/java/pe/edu/idat/app_cloud_pagos_client_ec2/controller/CloudController.java
+++ src/main/java/pe/edu/idat/app_cloud_pagos_client_ec2/controller/CloudController.java
@@ -1,6 +1,7 @@
 package pe.edu.idat.app_cloud_pagos_client_ec2.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +13,12 @@ public class CloudController {
     @GetMapping("/mensaje")
     public String verMensajeCloud(){
         return mensaje;
+    }
+
+
+    //EndPoint para recibir idPedido
+    @GetMapping("/realizar-pago/{idPedido}")
+    public String procesarPago(@PathVariable Long idPedido) {
+        return "Pago realizado con éxito para el pedido: " + idPedido;
     }
 }
