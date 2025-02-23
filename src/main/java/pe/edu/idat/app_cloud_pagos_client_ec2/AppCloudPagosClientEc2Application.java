c73268c7d97aaece1d0aeaf4367232acc26c6fcf
sandratriveno <sandra.triveno@gmail.com>
2025-02-23 11:19:27 -0500

    initial Project

---
+++ src/main/java/pe/edu/idat/app_cloud_pagos_client_ec2/AppCloudPagosClientEc2Application.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.app_cloud_pagos_client_ec2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCloudPagosClientEc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCloudPagosClientEc2Application.class, args);
+	}
+
+}
